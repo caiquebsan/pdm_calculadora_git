5fb1a91a22d95f41e18a4b0ae3fce60fa126755e
Caique Santos <caique.santos11@fatec.sp.gov.br>
2022-02-18 21:59:55 -0300

    Commit Soma

---
+++ CalculadoraGit.java
@@ -0,0 +1,14 @@
+
+public class CalculadoraGit {
+
+    public static void main(String[] args) {
+        int numero1 = 5;
+        int numero2 = 10;
+        int soma = numero1 + numero2;
+
+        System.out.println("Será realizado a soma dos dois numeros!");
+        System.out.println("Resultado = " + soma );
+    
+    }   
+
+}
